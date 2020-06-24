2250768f768f8efba6cb64cae64195658babb15a
Danielle Nicholson <daniellenicholsonart@gmail.com>
2020-06-24 15:56:03 -0600

    add ToUpper, ToLower, Substring, Length

--- Program.cs
+++ Program.cs
@@ -26,7 +26,15 @@ namespace c_sharp_intro
             // Instead of traditional concatenation using addition operator...
             //we can use string interpolation (placeholder) to embed valies in
             //a string
+            // https://docs.microsoft.com/en-us/dotnet/api/system.console.readline?view=netcore-3.1
             Console.WriteLine("hey {0}, you're looking good!", userName);
+
+           //lets's play with strings
+           Console.WriteLine("makemeuppercase".ToUpper() );
+           Console.WriteLine("MAKEMELOWERCASE".ToLower() );
+           // substring takes two arguments: 1. starting character positions 2. number of characters to collect.
+           Console.WriteLine("ABCDEFGHIJKLMNOPQRSTUVWXYZ".Substring( 3, 3 ) );
+        Console.WriteLine("XYZ".Length ); //get the number of characters for the string.
         }
     }
 }
