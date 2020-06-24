7d32a31af04066ef21d8b8d35e13cb6cfca1a710
Danielle Nicholson <daniellenicholsonart@gmail.com>
2020-06-24 15:13:32 -0600

    add an assignment; output concatenated hello string.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace c_sharp_intro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello tech buddies");
+            // two slashes are a single line comment 
+            //We are assigning a string value to a variable.
+            string myName = "Dani N";
+            /* We being multi-line comments witha forward slash and asterisk. 
+            
+            Symbols between values or evaluations are considered "operators" 
+            Below we are using the addition sysmbol to concatenate two strings.
+            Concatentation means gluing two strings, or text together
+            
+            .... and end multi-line comments forward slash and asterisk.*/
+    
+            Console.WriteLine("Hello " + myName!);
         }
     }
 }
---
+++ t
@@ -0,0 +1,11 @@
+[33mcommit 0c0de754d3b94f1fe960782f6f1b54c98ea29ef7[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: Danielle Nicholson <daniellenicholsonart@gmail.com>
+Date:   Wed Jun 24 14:13:26 2020 -0600
+
+    Update hello world text.
+
+[33mcommit 7c98d62e1f20b137e0c395ca4807d77828fc81bb[m
+Author: Danielle Nicholson <daniellenicholsonart@gmail.com>
+Date:   Wed Jun 24 12:06:31 2020 -0600
+
+    intial commit
