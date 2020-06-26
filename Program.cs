e2c8445ca5aad1637e99eb29bd7876799506ccd0
Danielle Nicholson <daniellenicholsonart@gmail.com>
2020-06-26 10:04:14 -0600

    add 2d array

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace c_sharp_intro
     {
         static void Main(string[] args)
         {
-           
+            // 2 dimensional array!
+            string[,] topics = { { "cat", "dog", "frog", "hedgehog"},
+                                    { "zebra", "giraffe", "lion", "e;ephant"},
+                                    { "stingray", "octopus", "shark", "whale"}};
+            // we can use foreach to iterate through the list of lists, one list at a time.
+            foreach (string topic in topics)
+
+            {
+                // output the current animal!
+                Console.WriteLine("this animal is: {0}", topic);
+            }
         }
     }
+
 }
